56f8361c275993aa689fb743d0d7ab0bd415a4a9
Saendhor <comix143.mariorossi@gmail.com>
2026-05-10 17:47:50 +0200

    fix: renamed the init suite; feat: added a ideas txt file; todo: translate queries to my ER diagram and implement them in sql

--- project/src/script_init.sh
+++ project/src/script_init.sh
@@ -1,7 +1,7 @@
 # This script is meant to initialize the project in sql.
 
 # Create Database and Tables
-mariadb -uuser -ppassword < create_tables.sql
+mariadb -uuser -ppassword < init/tables_create.sql
 
 # Populate the just created Tables
-mariadb -uuser -ppassword < populate_tables.sql
+mariadb -uuser -ppassword < init/tables_populate.sql
