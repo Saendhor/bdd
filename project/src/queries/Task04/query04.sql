ae814d585add09671bd3da4018aeb70282cddcbd
Saendhor <comix143.mariorossi@gmail.com>
2026-05-18 14:44:39 +0200

    feat: final commit

--- project/src/queries/Task04/query04.sql
+++ project/src/queries/Task04/query04.sql
@@ -1,5 +1,10 @@
 /*
-Inserisci consegna
+Visualizzare il numero complessivo di studenti che hanno
+preso parte ai laboratori fra il 2024 e il 2025
 */
 
-SELECT
+USE MuseoInterattivo;
+SELECT SUM(p.num_studenti) AS num_studenti_totale
+FROM Prenotazioni p
+    JOIN Assegnazioni_Laboratori al ON p.id_prenotazione = al.id_prenotazione
+WHERE data_prenotazione BETWEEN '2024-01-01' AND '2025-12-31';
--- project/src/queries/Task05/query05.sql
+++ project/src/queries/Task05/query05.sql
@@ -1,5 +1,13 @@
 /*
-Inserisci consegna
+Visualizzare il codice, nome e cognome di tutti gli operatori
+'GUIDE' che hanno preso parte ad almeno un laboratorio nel
+mese di aprile del 2025
 */
 
-SELECT
+USE MuseoInterattivo;
+SELECT o.id_operatore, o.nome, o.cognome
+FROM Assegnazioni_Laboratori al
+    JOIN Operatori o ON al.id_operatore = o.id_operatore
+    JOIN Prenotazioni p ON al. id_prenotazione = p.id_prenotazione
+WHERE o.ruolo = 'GUIDE'
+AND p.data_prenotazione BETWEEN '2025-04-01' AND '2025-04-31';
